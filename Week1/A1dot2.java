c3d1f2f1e59a1a014814642468531eff1e56bc40
Daniel Guzman <guz16005@gmail.com>
2019-08-18 15:51:19 -0600

    week1

---
+++ Week1/A1dot2.java
@@ -0,0 +1,11 @@
+public class A1dot2 {
+
+    public static void main(String args[]){
+
+        for(int i = 0; i < 5; i++){
+
+            System.out.println("Welcome to Java");
+        }
+
+    }
+}
